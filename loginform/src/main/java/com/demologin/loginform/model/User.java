a515e444a4707709fbee3cd87fb711e1c9694c7f
NandhiniGomathyNayaga <117257306+NandhiniGomathyNayaga@users.noreply.github.com>
2023-01-11 08:20:19 +0530

    Add files via upload

---
+++ loginform/src/main/java/com/demologin/loginform/model/User.java
@@ -0,0 +1,93 @@
+package com.demologin.loginform.model;
+
+import java.util.Collection;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
+import javax.persistence.JoinColumn;
+
+@Entity
+@Table(name= "user" , uniqueConstraints = @UniqueConstraint(columnNames = "email"))
+public class User {
+	 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	
+	@Column(name = "first_name")
+	private String firtName;
+	
+	@Column(name = "last_name")
+	private String lastName;
+	private String email;
+	private String password;
+	
+	@ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+	@JoinTable(
+			name = "users_roles",
+			joinColumns = @JoinColumn(
+					name = "user_id", referencedColumnName = "id"),
+			inverseJoinColumns = @JoinColumn(
+					name = "user-id", referencedColumnName = "id"))
+	
+	
+	private Collection<Role> roles;
+	
+	public User(String firtName, String lastName, String email, String password, Collection<Role> roles) {
+		super();
+		this.firtName = firtName;
+		this.lastName = lastName;
+		this.email = email;
+		this.password = password;
+		this.roles = roles;
+	}
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getFirtName() {
+		return firtName;
+	}
+	public void setFirtName(String firtName) {
+		this.firtName = firtName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public Collection<Role> getRoles() {
+		return roles;
+	}
+	public void setRoles(Collection<Role> roles) {
+		this.roles = roles;
+	}
+	
+	
+	
+
+}
---
+++ loginform/src/test/java/com/demologin/loginform/LoginformApplicationTests.java
@@ -0,0 +1,24 @@
+package com.demologin.loginform;
+
+import java.util.Scanner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginformApplicationTests {
+	public static void main(String args[]) {
+		int id;
+		String name;
+String  password;
+		Scanner s = new Scanner(System.in);
+		id=s.nextInt();
+		
+	}
+	
+	
+	void contextLoads() {
+	    
+	    	}
+	    }
+	
